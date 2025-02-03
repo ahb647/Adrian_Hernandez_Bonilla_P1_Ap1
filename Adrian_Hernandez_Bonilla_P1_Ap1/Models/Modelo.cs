21961f6146eaaaba9aea77cf0e814370b276ad70
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-02-03 18:42:12 -0400

    Creacion de la clase del modelo

---
+++ Adrian_Hernandez_Bonilla_P1_Ap1/Models/Modelo.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Adrian_Hernandez_Bonilla_P1_Ap1.Models
+{
+    public class Modelo
+    {
+        [Key]
+        public int Id { get; set; } 
+
+    }
+}

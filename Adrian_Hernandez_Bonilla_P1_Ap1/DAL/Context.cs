5ac1c07921a1787d8bab842ebdbc8d3cfbe021ad
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-02-10 22:01:56 -0400

    Contexto Creado

---
+++ Adrian_Hernandez_Bonilla_P1_Ap1/DAL/Context.cs
@@ -0,0 +1,16 @@
+﻿using Adrian_Hernandez_Bonilla_P1_Ap1.Models;
+using Microsoft.EntityFrameworkCore; 
+namespace Adrian_Hernandez_Bonilla_P1_Ap1.Context
+{
+
+public class Contexto : DbContext    
+    {
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Modelo> modelo { get; set; }
+    
+    }
+
+
+}
---
+++ Adrian_Hernandez_Bonilla_P1_Ap1/DAL/ContextDbFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Adrian_Hernandez_Bonilla_P1_Ap1.Context
+{
+    public class ContextoFactory : IDesignTimeDbContextFactory<Contexto>
+    {
+        public Contexto CreateDbContext(string[] args)
+        {
+            // Configura el DbContext usando la cadena de conexión de Somee.
+            var optionsBuilder = new DbContextOptionsBuilder<Contexto>();
+            optionsBuilder.UseSqlServer("workstation id=AdrianDB.mssql.somee.com;packet size=4096;user id=AHB_SQLLogin_1;pwd=419y65tsvd;data source=AdrianDB.mssql.somee.com;persist security info=False;initial catalog=AdrianDB;TrustServerCertificate=True");
+
+            return new Contexto(optionsBuilder.Options);
+        }
+    }
+}

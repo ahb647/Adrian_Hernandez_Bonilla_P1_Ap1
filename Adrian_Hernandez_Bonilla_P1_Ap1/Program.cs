bf0c21acb49f47d0aa1bad01a4e0d2e9e6a2cf58
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-02-10 22:12:33 -0400

    Contexto y Services Inyectados al programs

--- Adrian_Hernandez_Bonilla_P1_Ap1/Program.cs
+++ Adrian_Hernandez_Bonilla_P1_Ap1/Program.cs
@@ -1,3 +1,10 @@
+global using Microsoft.EntityFrameworkCore;
+global using Adrian_Hernandez_Bonilla_P1_Ap1.Context;
+global using Adrian_Hernandez_Bonilla_P1_Ap1.Models;
+global using Adrian_Hernandez_Bonilla_P1_Ap1.Services;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Options;
+
 using Adrian_Hernandez_Bonilla_P1_Ap1.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +14,19 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
+
+
+
+
+builder.Services.AddDbContextFactory<Contexto>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConStr"));
+});
+
+
+
+
+builder.Services.AddScoped<Service>();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())

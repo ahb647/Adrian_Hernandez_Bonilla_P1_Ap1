59f783a7b4da5f39b6bdb561e9cade4b7c68a6ba
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-02-17 20:52:35 -0400

    Por accidente se le coloco un limite a Fecha el cual correspondia para monto

--- Adrian_Hernandez_Bonilla_P1_Ap1/Models/Aportes.cs
+++ Adrian_Hernandez_Bonilla_P1_Ap1/Models/Aportes.cs
@@ -17,8 +17,10 @@ namespace Adrian_Hernandez_Bonilla_P1_Ap1.Models
         public string Observacion { get; set; }
 
         [Required(ErrorMessage = " El monto es obligatorio")]
-        public decimal Monto {  get; set; }
+
         [Range(0.01, double.MaxValue, ErrorMessage = "El Monto debe ser mayor a 0.")]
+        public decimal Monto {  get; set; }
+        
 
 
         public DateTime DateTime { get; set; }  = DateTime.Now; 
